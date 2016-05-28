535635e8da803f04de8e4b25ce194daae0e14ff0
Cong Min <i@congm.in>
2016-05-28 11:42:07 +0800

    blues

--- static/js/app.js
+++ static/js/app.js
@@ -263,7 +263,7 @@ $(function() {
                 if(element.nodeName === 'HTML'){
                     return -window.pageYOffset;
                 }
-                /* 偏移量(导航栏)为60 */
+                /* 偏移量(导航栏)为60 zhushi  */
                 return element.getBoundingClientRect().top - 60 + window.pageYOffset;
             },
             to: function(id){
